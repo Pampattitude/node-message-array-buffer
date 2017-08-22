ca3128c3e8f51d13b6167451dc41bc2333f951ac
gdelahodde <guillaume.delahodde@ogury.co>
2017-08-22 17:19:18 +0200

    much feature, such test, lots rename the project

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const defaultConfig = require('./lib/config');
 
-module.exports = class MessageBuffer {
+module.exports = class MessageArrayBuffer {
   constructor(options = {maxTime: defaultConfig.maxTime, maxSize: defaultConfig.maxSize}) {
     this.maxTime = options.maxTime || defaultConfig.maxTime;
     this.maxSize = options.maxSize || defaultConfig.maxSize;
 
-    this.elems = [];
     this.startTime = null;
+    this.elems = [];
+    this.savedElems_ = []; // Only for `popAllIfFull()` and `isFull()`
   }
 
   push(elem) {
@@ -18,18 +19,45 @@ module.exports = class MessageBuffer {
     // Return the whole array if any is true: maxTime or maxSize attained
     if ((new Date() - this.startTime) > this.maxTime) {
       this.startTime = null;
-      return this.popAll();
+      return this.popAll_();
     }
     if (this.maxSize <= this.elems.length)
-      return this.popAll();
+      return this.popAll_();
 
     return null;
   }
 
+  lookupAll() {
+    return this.elems;
+  }
+
   popAll() {
-    const ret = this.elems;
-    this.elems = [];
+    const ret = this.popAll_();
+    this.savedElems_ = [];
 
     return ret;
+  }
+
+  popAllIfFull() {
+    if (false === this.isFull())
+      return null;
+
+    const ret = this.savedElems_;
+    this.savedElems_ = [];
+
+    return ret;
+  }
+
+  isFull() {
+    return this.maxSize === this.savedElems_.length;
+  }
+
+  // private:
+
+  popAll_() {
+    this.savedElems_ = this.elems;
+    this.elems = [];
+
+    return this.savedElems_;
   }
 };
--- tests/index.js
+++ tests/index.js
@@ -2,51 +2,51 @@ const delay = require('delay');
 const expect = require('chai').expect;
 
 const config = require('../lib/config');
-const MessageBuffer = require('../index');
+const MessageArrayBuffer = require('../index');
 
-describe('MessageBuffer class', () => {
+describe('MessageArrayBuffer class', () => {
   it('should be set to default configuration', () => {
-    const mb = new MessageBuffer();
+    const mb = new MessageArrayBuffer();
 
     expect(mb.maxTime).to.be.equal(config.maxTime);
     expect(mb.maxSize).to.be.equal(config.maxSize);
   });
 
   it('should be set to default configuration with empty configuration', () => {
-    const mb = new MessageBuffer({});
+    const mb = new MessageArrayBuffer({});
 
     expect(mb.maxTime).to.be.equal(config.maxTime);
     expect(mb.maxSize).to.be.equal(config.maxSize);
   });
 
   it('should be set to custom configuration', () => {
-    const mb = new MessageBuffer({maxTime: 1, maxSize: 1});
+    const mb = new MessageArrayBuffer({maxTime: 1, maxSize: 1});
 
     expect(mb.maxTime).to.not.be.equal(config.maxTime);
     expect(mb.maxSize).to.not.be.equal(config.maxSize);
   });
 
   it('should `push()` a message to the buffer and return `null` with default configuration', () => {
-    const mb = new MessageBuffer({maxTime: 1000, maxSize: 1024});
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 1024});
 
     expect(mb.push({key: 'value'})).to.be.null;
   });
 
   it('should `push()` a message to the buffer and return the whole buffer with `maxSize === 1`', () => {
-    const mb = new MessageBuffer({maxTime: 1000, maxSize: 1});
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 1});
 
     expect(mb.push({key: 'value'})).to.have.lengthOf(1);
   });
 
   it('should `push()` two message to the buffer and return the whole buffer with `maxSize === 2`', () => {
-    const mb = new MessageBuffer({maxTime: 1000, maxSize: 2});
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 2});
 
     expect(mb.push({key1: 'value1'})).to.be.null;
     expect(mb.push({key2: 'value2'})).to.have.lengthOf(2);
   });
 
   it('should `push()` a message, wait 10ms, push again and return the whole buffer with `maxSize === 2` and `maxTime === 1`', async () => {
-    const mb = new MessageBuffer({maxTime: 1, maxSize: 2});
+    const mb = new MessageArrayBuffer({maxTime: 1, maxSize: 2});
 
     expect(mb.push({key1: 'value1'})).to.be.null;
     await delay(10);
@@ -54,10 +54,42 @@ describe('MessageBuffer class', () => {
   });
 
   it('should `push()` a message and return `null`, then return the whole buffer with `popAll()` only once', () => {
-    const mb = new MessageBuffer({maxTime: 1000, maxSize: 1024});
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 1024});
 
     expect(mb.push({key: 'value'})).to.be.null;
     expect(mb.popAll()).to.have.lengthOf(1);
     expect(mb.popAll()).to.have.lengthOf(0);
+  });
+
+  it('should `push()` a message and return `null`, then return the whole buffer with `lookupAll()` more than once', () => {
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 1024});
+
+    expect(mb.push({key: 'value'})).to.be.null;
+    expect(mb.lookupAll()).to.have.lengthOf(1);
+    expect(mb.lookupAll()).to.have.lengthOf(1);
+  });
+
+  it('should `push()` a message and return `null`, then return `true` with `isFull()` and an array of 1 with `popAllIfFull()` only once, with `maxSize === 1`', () => {
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 1});
+
+    expect(mb.push({key: 'value'})).to.have.lengthOf(1);
+    expect(mb.isFull()).to.be.equal(true);
+    expect(mb.popAllIfFull()).to.have.lengthOf(1);
+  });
+
+  it('should `push()` a message and return `null`, then return `true` with `isFull()` more than once with `maxSize === 1`', () => {
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 1});
+
+    expect(mb.push({key: 'value'})).to.have.lengthOf(1);
+    expect(mb.isFull()).to.be.equal(true);
+    expect(mb.isFull()).to.be.equal(true);
+  });
+
+  it('should `push()` a message and return `null`, then return `false` with `isFull()` and `null` with `popAllIfFull()` with `maxSize === 2`', () => {
+    const mb = new MessageArrayBuffer({maxTime: 1000, maxSize: 2});
+
+    expect(mb.push({key: 'value'})).to.be.null;
+    expect(mb.isFull()).to.be.equal(false);
+    expect(mb.popAllIfFull()).to.be.null;
   });
 });
